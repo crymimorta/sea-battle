54b94b5d7441570e3c70369596a6fce7e1ebfd81
Пользователь <bekture05@gmail.com>
2023-12-04 22:14:36 +0600

    Start placing ships

--- morskoy_boy.py
+++ morskoy_boy.py
@@ -7,3 +7,16 @@ def clear():
         a = system('cls')
     else:
         a = system('clear')
+
+def print_board(board):
+    print("   A B C D E F G")
+    for i in range(7):
+        print(f"{i + 1:2d} ", end="")
+        for j in range(7):
+            print(board[i][j], end=" ")
+        print()
+
+def place_ships():
+    ships = [(3, 's'), (2, 'm'), (2, 'm'), (1, 's'), (1, 's'), (1, 's'), (1, 's')]
+
+    board = [['O' for _ in range(7)] for _ in range(7)]
